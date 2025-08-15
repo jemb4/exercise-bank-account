45c3e48cc38ecd70bfd64c4857b6e632abc59099
Jesus Martin <jesus8.mb@gmail.com>
2025-08-15 19:12:54 +0200

    feat: deposit, withdraw, calculateMonthlyInterset, and bankaccount done

---
+++ src/main/java/dev/jesus/model/BankAccount.java
@@ -0,0 +1,48 @@
+package dev.jesus.model;
+
+public class BankAccount {
+
+  private float balance;
+  private int numberOfDeposits;
+  private int numberOfWithdrawals;
+  private float anualRate;
+  private float monthlyFee;
+
+  public BankAccount(float balance, float anualRate) {
+    this.balance = balance;
+    this.anualRate = anualRate;
+    numberOfDeposits = 0;
+    numberOfWithdrawals = 0;
+    monthlyFee = 0;
+  }
+
+  public void deposit(float amount) {
+    numberOfDeposits++;
+    balance += amount;
+  }
+
+  public void withdraw(float amount) {
+    if (balance < amount) {
+      System.out.println("No tienes suficiente saldo para hacer este retiro.");
+    }
+
+    if (balance >= amount) {
+      numberOfWithdrawals++;
+      balance -= amount;
+    }
+  }
+
+  public void calculateMonthlyInterest() {
+    float monthlyRate = anualRate / 12 / 100;
+    float monthlyInterest = monthlyRate * balance;
+    balance += monthlyInterest;
+  }
+
+  public void monthlyStatement() {
+
+  }
+
+  public void printAccountDetails() {
+
+  }
+}
